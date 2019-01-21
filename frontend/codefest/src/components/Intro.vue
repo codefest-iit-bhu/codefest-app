2a1b7fc07d0c6c04cc70a9c722f54de165dc2863
Prabhash Kumar <prabhash.github@gmail.com>
2019-01-21 23:28:51 +0530

    Added Intro and Lookback

--- frontend/codefest/src/components/Intro.vue
+++ frontend/codefest/src/components/Intro.vue
@@ -1,7 +1,14 @@
 <template>
-	<header :class="$style.intro">
-		<slot />
-	</header>
+	<div :class="$style.intro">
+		<h1>A Byte About Us</h1>
+		<p>	The Department of Computer Science and Engineering, IIT (BHU) Varanasi 
+			brings to you yet another edition of its annual coding extravaganza, Codefest!
+			Being more diverse than ever, Codefest '18 boasts of a plethora of events, 
+			ranging from competitive programming, algorithms and application development 
+			to upcoming trends like cryptography, machine learning, computer vision and cyber security.
+			With problems being of varying difficulty levels, Codefest provides the perfect platform 
+			for fresh enthusiasts, as well as the experienced ones, to code together and compete for ultimate glory.</p>
+	</div>
 </template>
 
 <script>
@@ -13,17 +20,23 @@
 <style module lang="styl">
 	.intro
 		padding 24px
-		background var(--white)
+		background black
 		border-radius var(--radius)
 		box-shadow 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)
 		margin-bottom 32px
+		font-family "Aldo the Apache"
+		font-size 20px
 	
 		h1
+			color #86ff00
 			margin-bottom 16px
+			text-align center
+			padding 16px
 	
 		p
 			margin-bottom 12px
 			line-height 1.35
+			color #86ff00
 			&:last-of-type
 				margin-bottom 0
 </style>
--- frontend/codefest/src/components/Lookback.vue
+++ frontend/codefest/src/components/Lookback.vue
@@ -1,6 +1,122 @@
 <template>
-  <div>Lookback</div>
+	<div :class="$style.lookback">
+    	<h1>LOOKBACK</h1>
+		<div :class="$style.cell" v-for="(stat, i) in stats" :key="i">
+			<div :class="$style.clip">
+				<img :src="stat.image" :class="$style.img" />
+			</div>
+			<div :class="$style.txt">{{ stat.name }}<br>{{ stat.value }}</div>
+		</div><br>
+		
+		<!-- <svg class="clip-svg">
+			<defs>
+				<clipPath id="hexagon-clip" clipPathUnits="objectBoundingBox">
+					<polygon points="0.25 0.05, 0.75 0.05, 1 0.5, 0.75 0.95, 0.25 0.95, 0 0.5" />
+				</clipPath>
+			</defs>
+		</svg>	 -->
+	</div>
 </template>
 <script>
-export default {};
+export default {
+	data() {
+		return {
+			stats: [
+				{
+					name: 'Prize Money',
+					value: '500,000',
+					image: '../assets/Lookback/25473.png'
+				},
+				{
+					name: 'Countries',
+					value: '98',
+					image: '../assets/Lookback/mobile-test-studio-locations-icon.png'
+				},
+				{
+					name: 'Participants',
+					value: '24,654',
+					image: '../assets/Lookback/kisspng-computer-icons-internet-forum-discussion-group-blo-5aff15db0bb4c8.818433001526666715048.png'
+				},
+				{
+					name: 'Unique Visitors',
+					value: '328,902',
+					image: '../assets/Lookback/visitor.png'
+				},
+				{
+					name: 'Registrations',
+					value: '16,302',
+					image: '../assets/Lookback/edit-user-male.png'
+				}
+			]
+		}
+	}
+};
 </script>
+
+<style module lang="styl">
+	.lookback
+		padding 24px
+		background black
+		border-radius var(--radius)
+		box-shadow 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)
+		margin-bottom 32px
+		font-family "Aldo the Apache"
+		font-size 20px
+
+		.cell
+			width 500px
+			height 100px
+			margin-left auto
+			margin-right auto
+
+			.txt
+				color #86ff00
+				position relative
+				top -70%
+
+			.clip
+				clip-path: polygon(25% 5%, 75% 5%, 100% 50%, 75% 95%, 25% 95%, 0% 50%);
+				background white
+				width 100px
+				height 100px
+				background #57a300
+
+			.img
+				width 80%
+				height 80%
+				position relative
+				top -25%
+				left 10%
+
+			&:nth-child(2)
+				.img
+					top 0%
+					left 0%
+					width: 80%
+					height: 80%
+					margin: 10px
+
+			&:nth-child(odd)
+				.txt
+					margin-right 55%
+					text-align right
+					float right
+				
+				.clip
+					margin-left 50%
+
+			&:nth-child(even)
+				.txt
+					margin-left 55%
+					text-align left
+					float left
+				
+				.clip
+					margin-left 30%
+	
+		h1
+			color #86ff00
+			margin-bottom 16px
+			text-align center
+			padding 16px
+</style>
