212c40f29daff8151eb52290d2855fdda5e63923
Piyush Maurya <piyushkumarmaurya@gmail.com>
2019-01-23 13:35:12 +0530

    0s and 1s instead of code

--- frontend/codefest/src/components/Hero.vue
+++ frontend/codefest/src/components/Hero.vue
@@ -21,9 +21,9 @@
 			var ctx = c.getContext("2d");
 			c.height = window.innerHeight;
 			c.width = 2*window.innerWidth;
-			var code = "{}-+=xyz*&%print$;':<>?/";
+			var code = "01";
 			code = code.split("");
-			var font_size = 40;
+			var font_size = 35;
 			var columns = 2*c.width/font_size;
 			var drops = [];
 			for(var x = 0; x < columns; x++) drops[x] = 1; 
@@ -31,7 +31,7 @@
 				ctx.fillStyle = "rgba(0, 0, 0, 0.03)";
 				ctx.fillRect(0, 0, c.width, c.height);
 				ctx.fillStyle = "#86ff00";
-				ctx.font = font_size + "px Courier New";
+				ctx.font = font_size + "px arial";
 				for(var i = 0; i < drops.length; i++){
 					var text = code[Math.floor(Math.random()*code.length)];
 					ctx.fillText(text, i*font_size, drops[i]*font_size);
@@ -42,7 +42,7 @@
 				}
 			}
 
-			setInterval(draw, 50);
+			setInterval(draw, 30);
 
 		}
 	}
