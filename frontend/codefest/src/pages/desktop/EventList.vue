5533542b71659b526a27ed05b0e2b4476101aee4
Shivansh Saini <shivanshs9@gmail.com>
2019-02-02 17:52:26 +0530

    Merged Events.

--- frontend/codefest/src/pages/desktop/EventList.vue
+++ frontend/codefest/src/pages/desktop/EventList.vue
@@ -1,8 +1,42 @@
 <template>
-  <div></div>
+  <div :class="$style.root">
+    <Events/>
+    <Footer/>
+  </div>
 </template>
+
 <script>
-export default {};
+import AppBar from "@components/AppBar";
+import Events from "@components/Events";
+import Terminal from "@components/Terminal";
+import Footer from "@components/Footer";
+export default {
+  components: {
+    AppBar,
+    Events,
+    Terminal,
+    Footer
+  }
+};
 </script>
 <style module lang="stylus">
+.wrapper {
+  width: 100%;
+  margin: 0 auto;
+  position: relative;
+  min-height: calc(55vh - 16px);
+  z-index: 1;
+  font-family: 'Roboto Mono';
+  font-size: 18px;
+}
+
+.root {
+  height: 100%;
+}
+
+@media screen and (max-width: 769px) {
+  .wrapper {
+    width: 90%;
+  }
+}
 </style>
