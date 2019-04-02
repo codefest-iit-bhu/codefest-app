e07430364afe9270be7ba0220158153d03f85b19
Prabhash Kumar <prabhash.github@gmail.com>
2019-04-03 02:35:45 +0530

    Haxplore About done.

---
+++ src/components/haxplore/About.vue
@@ -0,0 +1,73 @@
+<template>
+  <SectionLayout title="About" id="about">
+    <ResponsiveTwoColumnLayout :isRightAbove="true">
+      <div :class="$style.description" slot="left">
+        <p>
+          <span>HaXplore</span> is going to be the ultimate pit-stop for developers.
+          From taking care of your accomodation to providing you with savoury treats; food facilities and refreshments throughout the competition.
+          Don't worry about your travel costs as well, we'll have them covered for you!
+          <br>
+          <br>And even if you don't win anything, fret not my friend!
+          You'll go home with loads of goodies, a lot of experience and memories for a lifetime.
+        </p>
+      </div>
+      <div :class="$style.video" slot="right">
+        <youtube :video-id="videoId" ref="youtube" @playing="playing"></youtube>
+      </div>
+    </ResponsiveTwoColumnLayout>
+  </SectionLayout>
+</template>
+
+
+<script>
+import SectionLayout from "@components/layouts/SectionLayout";
+import ResponsiveTwoColumnLayout from "@components/layouts/ResponsiveTwoColumnLayout";
+
+export default {
+  components: {
+    ResponsiveTwoColumnLayout,
+    SectionLayout
+  },
+
+  data() {
+    return {
+      videoId: "4I0AUhHTR-g"
+    };
+  },
+  methods: {
+    playing() {
+      console.log("Dekho Dekho!!");
+    }
+  },
+  computed: {
+    player() {
+      return this.$refs.youtube.player;
+    }
+  }
+};
+</script>
+
+<style module lang="stylus">
+@require '~@styles/theme';
+@require '~@styles/mixins';
+
+.description {
+  text-align: left;
+  margin: 30px 30px 0 0;
+
+  span {
+    font-weight: bold;
+  }
+}
+
+.video {
+  box-shadow: 0 0 10px $chartreuse;
+  width: 100%;
+
+  & > iframe {
+    width: 100%;
+  }
+}
+</style>
+
+
