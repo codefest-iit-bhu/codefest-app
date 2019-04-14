0a1f528e914f94a69cd02b1622f93282033d6981
Prabhash Kumar <prabhash.github@gmail.com>
2019-04-14 16:57:40 +0530

    Profile done.

---
+++ src/components/DashboardProfile.vue
@@ -0,0 +1,223 @@
+<template>
+  <div :class="$style.root">
+    <main :class="$style.wrapper">
+      <SectionLayout title="My Profile">
+        <div :class="$style.profile">
+          <ResponsiveTwoColumnLayout :isRightAbove="true">
+            <div :class="$style.about" slot="left">
+              <!-- <div :class="$style.card" v-for="(item ,i) in this.items" :key="i">
+              <div :class="$style.label">{{item.key}}</div>
+              <div :class="$style.value">{{item.value}}</div>
+              </div>-->
+              <div :class="$style.box">
+                <span :class="$style.key">Referral Code</span>
+                <span :class="$style.value">{{profile.referral_code}}</span>
+                <i class="fas fa-copy" :class="$style.copyIcon" @click="clickToCopy"></i>
+              </div>
+            </div>
+            <div :class="$style.dp" slot="right">
+              <div :class="$style.img">
+                <span class="absolute-center">{{retrieveInitials}}</span>
+              </div>
+              <span :class="$style.name">{{profile.name}}</span>
+            </div>
+          </ResponsiveTwoColumnLayout>
+        </div>
+
+        <div :class="$style.link">
+          <a href="/edit-profile" :class="$style.linkText">
+            <h4>Edit Profile</h4>
+          </a>
+        </div>
+      </SectionLayout>
+    </main>
+  </div>
+</template>
+
+<script>
+import { copyToClipboard } from "@js/utils";
+
+import SectionLayout from "@components/layouts/SectionLayout";
+import ResponsiveTwoColumnLayout from "@components/layouts/ResponsiveTwoColumnLayout";
+export default {
+  components: {
+    SectionLayout,
+    ResponsiveTwoColumnLayout
+  },
+  computed: {
+    // items() {
+    //   return [
+    // {
+    //   key: "Name",
+    //   value: this.profile.name
+    // },
+    // {
+    //   key: "Gender",
+    //   value: this.profile.gender
+    // },
+    // {
+    //   key: "Degree",
+    //   value: this.profile.degree
+    // },
+    // {
+    //   key: "Branch",
+    //   value: this.profile.branch
+    // },
+    // {
+    //   key: "Year Of Study",
+    //   value: this.profile.study_year
+    // },
+    // {
+    //   key: "Institute",
+    //   value: this.profile.institute
+    // },
+    // {
+    //   key: "Country",
+    //   value: this.profile.country
+    // },
+    // {
+    //   key: "Phone",
+    //   value: this.profile.phone
+    // },
+    // {
+    //   key: "Referral Code",
+    //   value: this.profile.referral_code
+    // }
+    // ];
+    // },
+    retrieveInitials() {
+      const { name } = this.profile;
+      const newName = name.split(/\s+/);
+      return newName[0][0] + newName[1][0];
+    }
+  },
+  props: {
+    profile: {
+      required: true,
+      type: Object
+    }
+  },
+  methods: {
+    clickToCopy: function() {
+      copyToClipboard(this.profile.referral_code);
+    }
+  }
+};
+</script>
+
+<style module lang="stylus">
+@require '~@styles/theme';
+
+.root {
+  height: 100%;
+}
+
+.wrapper {
+  width: 80%;
+  margin: 0 auto;
+  padding: 50px 0;
+  z-index: 1;
+  position: relative;
+  font: 500 18px 'Quicksand';
+  font-kerning: auto;
+}
+
+.link {
+  margin: 40px auto;
+  width: 290px;
+  height: auto;
+  padding: 24px;
+  border-radius: 50px;
+  border: 2px solid $chartreuse;
+  text-align: center;
+  cursor: pointer;
+
+  .linkText {
+    color: $chartreuse;
+    display: inline;
+    text-decoration: none;
+
+    h4 {
+      font-family: 'Aldo the Apache';
+      font-size: 30px;
+      margin: 0;
+    }
+  }
+
+  &:hover {
+    box-shadow: inset 0px 0px 10px $chartreuse;
+  }
+}
+
+.profile {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+
+  .about {
+    text-align: center;
+    margin: 15px;
+    width: inherit;
+
+    .box {
+      border: 2px solid $chartreuse;
+      border-radius: 20px;
+      text-align: center;
+      height: 50px;
+      width: 100%;
+
+      .copyIcon {
+        cursor: pointer;
+        margin-right: 5px;
+        margin-top: 10px;
+        float: right;
+      }
+
+      .key {
+        display: inline-block;
+        float: left;
+        font-weight: bold;
+        font-size: 24px;
+        padding: 15px;
+        height: 100%;
+        border-radius: inherit;
+        border-bottom-right-radius: 0;
+        border-top-right-radius: 0;
+        border-right: 1px solid $chartreuse;
+        background: alpha($chartreuse, 0.7);
+      }
+
+      .value {
+        display: inline-block;
+        font-size: 14px;
+        padding: 15px;
+        height: 100%;
+      }
+    }
+  }
+
+  .dp {
+    text-align: center;
+
+    .img {
+      margin: 15px auto;
+      position: relative;
+      background-image: url('@assets/dp.png');
+      background-size: cover;
+      width: 150px;
+      height: 150px;
+      font-size: 30px;
+      font-weight: bold;
+    }
+
+    .name {
+      float: left;
+      font-size: 30px;
+      font-weight: bold;
+      width: 100%;
+    }
+  }
+}
+</style>
+
+
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,22 +1,43 @@
 <template>
-  <div :class="$style.root">Dashboard</div>
+  <div :class="$style.root">
+    <h1>Dashboard</h1>
+    <DashboardProfile :profile="profile"></DashboardProfile>
+  </div>
 </template>
 
 <script>
 import API from "@js/api";
-
+import DashboardProfile from "@components/DashboardProfile";
 export default {
+  components: {
+    DashboardProfile
+  },
+  data() {
+    return {
+      profile: {}
+    };
+  },
+
   created() {
     API.fetch("profile/", { token: this.$store.getters.authToken })
       .then(({ data }) => {
         if (!data.is_profile_complete) {
-          alert("Not Complete!");
+          console.log("Not Complete!");
         }
         console.log(data);
+        this.profile = data;
       })
       .catch(console.log);
   }
 };
 </script>
 
-<style module lang="stylus"></style>
+<style module lang="stylus">
+.root {
+  height: 100%;
+
+  h1 {
+    color: white;
+  }
+}
+</style>
