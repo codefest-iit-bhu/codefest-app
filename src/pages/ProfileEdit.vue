11bff76c3b63a2492b9e1e299fd41a1fa3c7eab3
Piyush Maurya <piyushkumarmaurya@gmail.com>
2019-04-23 23:04:37 +0530

    Fixes in institutes and branch lists.

--- src/pages/ProfileEdit.vue
+++ src/pages/ProfileEdit.vue
@@ -75,18 +75,25 @@
                     <span v-if="profile.institute_type==2">last institute</span>
                     <span v-else>institute</span>
                   </label>
-                  <input type="text" id="institute_name" v-model="profile.institute_name" required>
+                  <input
+                    type="text"
+                    id="institute_name"
+                    placeholder="Type out your institute"
+                    v-model="profile.institute_name"
+                    required
+                  >
                 </div>
                 <div :class="$style.field">
                   <label for="study_year" :class="$style.label">
                     <span v-if="profile.institute_type==0">class</span>
-                    <span v-if="profile.institute_type==1">year</span>
+                    <span v-if="profile.institute_type==1">study year</span>
                     <span v-if="profile.institute_type==2">experience</span>
                   </label>
                   <input
                     type="number"
                     id="study_year"
                     name="study_year"
+                    placeholder="(e.g. 2)"
                     min="1"
                     v-model="profile.study_year"
                     required
@@ -94,7 +101,13 @@
                 </div>
                 <div :class="$style.field" v-show="profile.institute_type==1">
                   <label for="branch" :class="$style.label">branch</label>
-                  <input type="text" id="branch" name="branch" v-model="profile.branch">
+                  <input
+                    type="text"
+                    id="branch"
+                    name="branch"
+                    placeholder="Type out your branch."
+                    v-model="profile.branch"
+                  >
                 </div>
                 <div :class="$style.field" v-show="profile.institute_type!=0">
                   <label for="degree" :class="$style.label">degree</label>
@@ -258,15 +271,11 @@ export default {
               option.innerHTML = item[datafield];
               dataList.appendChild(option);
             });
-
-            input.placeholder = "";
           } else {
-            input.placeholder = "error";
+            console.log("error");
           }
         }
       };
-
-      input.placeholder = "";
 
       request.open("GET", url, true);
       request.send();
@@ -342,7 +351,7 @@ export default {
 }
 
 .wrapper {
-  width: 80%;
+  width: 95%;
   margin: 0 auto;
   padding: 100px 0;
   position: relative;
@@ -357,7 +366,7 @@ export default {
 }
 
 .form {
-  margin: 20px;
+  margin: 0;
   position: relative;
   border: 1px solid $chartreuse;
   border-radius: 5px;
@@ -473,6 +482,7 @@ export default {
       .label {
         display: inline-block;
         text-align: left;
+        text-transform: capitalize;
         width: 100px;
         color: white;
         height: 30px;
