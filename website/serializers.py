6299d16fbf8a9e8696235104f2c71711eadcf7d9
Amritansh Singhal <amritansh.singhal.cse17@itbhu.ac.in>
2019-05-13 22:52:21 +0530

    Added ID to CA view

--- website/serializers.py
+++ website/serializers.py
@@ -201,4 +201,4 @@ class ResumeSerializer(serializers.ModelSerializer):
 class CALeaderboardSerializer(serializers.ModelSerializer):
     class Meta:
         model=CA
-        exclude = ('id',)
+        fields = '__all__'
