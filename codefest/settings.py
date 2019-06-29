1445077a2edd88ba38913ead11bdb32f15ee26b2
Amritansh Singhal <amritansh.singhal.cse17@itbhu.ac.in>
2019-06-29 19:54:56 +0530

    Switched off persistent database connections

--- codefest/settings.py
+++ codefest/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
+DATABASES['default'].update(dj_database_url.config(conn_max_age=0))
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
