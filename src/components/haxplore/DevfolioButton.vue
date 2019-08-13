a340e8b0d84a8d4e22daf18f82462b46b3c6776d
Prabhash Kumar <prabhash.github@gmail.com>
2019-08-13 15:56:53 +0530

    Added Timeline Elements and Cisco as a Sponsor.

--- src/components/haxplore/DevfolioButton.vue
+++ src/components/haxplore/DevfolioButton.vue
@@ -1,20 +1,8 @@
 <template>
-  <div :class="[$style.floatBtn, animateClass, $style[$mq]]">
-    <button id="devfolio-apply-now" ref="devfolioBtn" @click="registerDefault">
-      <svg
-        class="logo"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 115.46 123.46"
-        style="height:24px;width:24px;margin-right:8px;"
-      >
-        <path
-          d="M115.46 68a55.43 55.43 0 0 1-50.85 55.11S28.12 124 16 123a12.6 12.6 0 0 1-10.09-7.5 15.85 15.85 0 0 0 5.36 1.5c4 .34 10.72.51 20.13.51 13.82 0 28.84-.38 29-.38h.26a60.14 60.14 0 0 0 54.72-52.47c.051.05.08 2.18.08 3.34z"
-        ></path>
-        <path
-          d="M110.93 55.87A55.43 55.43 0 01 60.08 111s-36.48.92-48.58-.12C5 110.29.15 104.22 0 97.52l.2-83.84C.38 7 5.26.94 11.76.41c12.11-1 48.59.12 48.59.12a55.41 55.41 0 0 1 50.58 55.34z"
-        ></path>
-      </svg>Apply with Devfolio
-    </button>
+  <div :class="$style.link">
+    <div :class="$style.linkText">
+      <h4>Applications Closed</h4>
+    </div>
   </div>
 </template>
 
@@ -54,6 +42,33 @@ export default {
 $btn-height = 65px;
 $btn-width = 250px;
 
+.link {
+  margin: auto;
+  width: 290px;
+  height: auto;
+  padding: 24px;
+  border-radius: 50px;
+  border: 2px solid $chartreuse;
+  text-align: center;
+  cursor: pointer;
+
+  .linkText {
+    color: $white;
+    display: inline;
+    text-decoration: none;
+
+    h4 {
+      font-family: 'Viga';
+      font-size: 20px;
+      margin: 0;
+    }
+  }
+
+  &:hover {
+    box-shadow: inset 0px 0px 10px $chartreuse;
+  }
+}
+
 .floatBtn {
   /* stickWithSpace(bottom: 30px, right: 30px); */
   width: $btn-width;
@@ -67,7 +82,6 @@ $btn-width = 250px;
   color: $chartreuse;
   font-family: 'Roboto Slab';
   box-shadow: inset 0px 0px 5px $chartreuse;
-  text-align: center;
   overflow: hidden;
 
   &:hover {
@@ -79,21 +93,21 @@ $btn-width = 250px;
       -ms-flex-align: center;
       -webkit-box-align: center;
       padding: 10px 20px;
-      align-items: center;
+      /* align-items: center; */
       background: none;
-      cursor: pointer;
+      /* cursor: pointer; */
       color: $white;
       border: none;
       display: -ms-flexbox;
       display: -webkit-box;
-      display: flex;
+      /* display: flex; */
       font-size: 18px;
       font-weight: 600;
       text-align: center;
       text-transform: none;
 
       &:not([disabled]) {
-        cursor: pointer;
+        /* cursor: pointer; */
       }
 
       .logo {
--- src/components/haxplore/Timeline.vue
+++ src/components/haxplore/Timeline.vue
@@ -3,9 +3,9 @@
     <div :class="[$style.timelineContainer, $style[$mq]]">
       <ResponsiveTwoColumnLayout :isRightAbove="true">
         <div :class="$style.timelineHeader" slot="right">
-          <img src="@assets/haxplore/timeline.svg">
+          <img src="@assets/haxplore/timeline.svg" />
         </div>
-        <Timeline :timeline="timeline" slot="left"/>
+        <Timeline :timeline="timeline" slot="left" />
       </ResponsiveTwoColumnLayout>
     </div>
   </SectionLayout>
@@ -14,7 +14,8 @@
 <script>
 const Timeline = () => import("@components/Timeline");
 const SectionLayout = () => import("@components/layouts/SectionLayout");
-const ResponsiveTwoColumnLayout = () => import("@components/layouts/ResponsiveTwoColumnLayout");
+const ResponsiveTwoColumnLayout = () =>
+  import("@components/layouts/ResponsiveTwoColumnLayout");
 
 export default {
   components: {
@@ -29,20 +30,126 @@ export default {
           isMajor: true,
           start: new Date(2019, 7, 23, 12, 0),
           end: null,
-          title: "TBA",
+          title: "",
+          summary: ""
+        },
+        {
+          isMajor: false,
+          start: new Date(2019, 7, 23, 20, 0),
+          end: null,
+          title: "Reporting @ Visvesvaraya Hostel",
           summary: ""
         },
         {
           isMajor: true,
           start: new Date(2019, 7, 24, 12, 0),
           end: null,
-          title: "TBA",
+          title: "",
+          summary: ""
+        },
+
+        {
+          isMajor: false,
+          start: new Date(2019, 7, 24, 9, 0),
+          end: null,
+          title: "Breakfast @ CSE Department",
+          summary: ""
+        },
+
+        {
+          isMajor: false,
+          start: new Date(2019, 7, 24, 10, 0),
+          end: null,
+          title: "Theme Announcement & Opening Ceremony @ LT 21",
+          summary: ""
+        },
+
+        {
+          isMajor: false,
+          start: new Date(2019, 7, 24, 12, 0),
+          end: null,
+          title: "Lunch @ CSE Department",
+          summary: ""
+        },
+
+        {
+          isMajor: false,
+          start: new Date(2019, 7, 24, 12, 45),
+          end: null,
+          title: "Venue Reporting @ UG Lab-I, CSE Department",
+          summary: ""
+        },
+
+        {
+          isMajor: false,
+          start: new Date(2019, 7, 24, 13, 0),
+          end: null,
+          title: "EVENT START",
+          summary: ""
+        },
+
+        {
+          isMajor: false,
+          start: new Date(2019, 7, 24, 17, 30),
+          end: null,
+          title: "Snacks/Supper",
+          summary: ""
+        },
+
+        {
+          isMajor: false,
+          start: new Date(2019, 7, 24, 21, 0),
+          end: null,
+          title: "Dinner",
+          summary: ""
+        },
+        {
+          isMajor: false,
+          start: new Date(2019, 7, 24, 22, 0),
+          end: null,
+          title: "Mid-Review",
           summary: ""
         },
         {
           isMajor: true,
           start: new Date(2019, 7, 25, 12, 0),
-          title: "TBA",
+          title: "",
+          summary: ""
+        },
+        {
+          isMajor: false,
+          start: new Date(2019, 7, 25, 2, 0),
+          title: "Snacks",
+          summary: ""
+        },
+        {
+          isMajor: false,
+          start: new Date(2019, 7, 25, 9, 0),
+          title: "Breakfast",
+          summary: ""
+        },
+        {
+          isMajor: false,
+          start: new Date(2019, 7, 25, 12, 0),
+          title: "Submission Deadline",
+          summary: ""
+        },
+        {
+          isMajor: false,
+          start: new Date(2019, 7, 25, 12, 0),
+          title: "Lunch",
+          summary: ""
+        },
+        {
+          isMajor: false,
+          start: new Date(2019, 7, 25, 17, 0),
+          title: "Presentation",
+          summary: ""
+        },
+        {
+          isMajor: false,
+          start: new Date(2019, 7, 25, 19, 30),
+          title: "Closing Ceremony",
           summary: ""
         }
       ]
