d168cc4fe09b5a6e30d4e90c5758e1380d5f898d
Adarsh Gupta <adarshgupta.cse18@itbhu.ac.in>
2020-03-30 02:48:01 +0530

    changes

--- src/components/Sponsors.vue
+++ src/components/Sponsors.vue
@@ -32,7 +32,7 @@
       </div>
       <div :class="$style.txt"></div>
       <div :class="$style.link">
-        <a href="https://goo.gl/forms/RyjmY7i002oUHivu2" :class="$style.linkText" target="_blank">
+        <a href="https://drive.google.com/open?id=15BYqDZwShgFFqkgBPKqr0j-AcFF-_Umm" :class="$style.linkText" target="_blank">
           <h4>Sponsor Us</h4>
         </a>
       </div>
@@ -54,18 +54,7 @@ export default {
   },
   data() {
     return {
-      stats: [
-        {
-          image: "assets/Sponsors/cisco.png",
-          text: "Associate Sponsor",
-          link: "https://www.cisco.com/"
-        },
-        {
-          image: "assets/Sponsors/mozilla1.png",
-          text: "Diversity Partner",
-          link: "https://www.mozilla.org/"
-        }
-      ],
+      stats: [],
       title: {
         image: "assets/Sponsors/aws.png",
         link: "https://aws.amazon.com/"
