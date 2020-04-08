e20ea70e0e3894230f334771a5490cebd6d5cfc5
Shivansh Saini <shivanshs9@gmail.com>
2020-04-08 22:55:56 +0530

    Updated manifest to Codefest2020
    - Cleaned unused assets
    - Fixed lookback warning

--- src/components/Lookback.vue
+++ src/components/Lookback.vue
@@ -1,9 +1,14 @@
 <template>
   <div :class="[$style.lookback, $style[$mq]]">
     <GridLayout :itemWidth="computedWidth">
-      <div :class="$style.stat" v-for="(stat, i) in this.stats" :key="i" :slot="`item${i}`">
+      <div
+        :class="$style.stat"
+        v-for="(stat, i) in this.stats"
+        :key="i"
+        :slot="`item${i}`"
+      >
         <div :class="$style.clip">
-          <img :src="stat.image" :class="$style.img">
+          <img :src="stat.image" :class="$style.img" />
         </div>
         <div :class="$style.name">{{ stat.name }}</div>
         <div :class="$style.value">{{ stat.value }}</div>
@@ -17,49 +22,18 @@ import GridLayout from "./layouts/GridLayout";
 export default {
   props: {
     stats: {
-      type: Array
-    }
+      type: Array,
+    },
   },
   components: {
-    GridLayout
+    GridLayout,
   },
   computed: {
     computedWidth() {
       if (this.$mq == "lg") return "160px";
       return "220px";
-    }
+    },
   },
-  data() {
-    return {
-      stats: [
-        {
-          name: "Prize Money",
-          value: "650,000",
-          image: "assets/Lookback/lb_prize.svg"
-        },
-        {
-          name: "Countries",
-          value: "104",
-          image: "assets/Lookback/lb_countries.svg"
-        },
-        {
-          name: "Participants",
-          value: "29,800",
-          image: "assets/Lookback/lb_participant.svg"
-        },
-        {
-          name: "Unique Visitors",
-          value: "664,300",
-          image: "assets/Lookback/lb_visitor.svg"
-        },
-        {
-          name: "Registrations",
-          value: "23,000",
-          image: "assets/Lookback/lb_registration.svg"
-        }
-      ]
-    };
-  }
 };
 </script>
 
