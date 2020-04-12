06015b4569ccb25c300fe7099c93bbba011bcd44
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-04-12 19:32:05 +0530

    fixed clear command issue by making a new NullOutput class

--- src/js/commands.js
+++ src/js/commands.js
@@ -51,6 +51,12 @@ class BaseOutput extends Object {
   }
 }
 
+class NullOutput extends Object {
+  html() {
+    return null
+  }
+}
+
 class ColumnOutput extends BaseOutput {
   processData(envs) {
     let vue = envs.vue;
@@ -128,10 +134,7 @@ class ClearCommand extends BaseCommand {
 
   run() {
     terminal.clearHistory();
-    return new BaseOutput(
-      `clear`,
-      this.envs
-    );
+    return new NullOutput()
   }
 }
 
