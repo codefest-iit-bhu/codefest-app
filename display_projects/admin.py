e6c973c2393971fd4ecda79bb3aea7353f8a49db
Shale Arkane <shalearkane@gmail.com>
2021-12-06 13:10:29 +0530

    added comments model for projects

--- display_projects/admin.py
+++ display_projects/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import ProjectModel
+from .models import ProjectModel, ProjectRating, ProjectComment
 
 # Register your models here.
 admin.site.register(ProjectModel)
+admin.site.register(ProjectRating)
+admin.site.register(ProjectComment)
--- display_projects/models.py
+++ display_projects/models.py
@@ -5,6 +5,7 @@ from django.db.models.deletion import CASCADE
 
 
 class ProjectModel(models.Model):
+    # TODO: change to teams later
     creator = models.ForeignKey(
         User, blank=True, null=True, related_name="creator", on_delete=models.CASCADE)
     title = models.CharField(max_length=20)
@@ -31,4 +32,17 @@ class ProjectRating(models.Model):
         unique_together = ("user", "project")
 
     def __str__(self) -> str:
-        return self.user.username + " - " + self.project.title + str(self.rating)
+        return 'Rating \'{}\' by \'{}\' on \'{}\''.format(self.rating, self.user, self.project)
+
+
+class ProjectComment(models.Model):
+    user = models.ForeignKey(
+        User, related_name='user_comment', on_delete=models.CASCADE)
+    project = models.ForeignKey(
+        ProjectModel, related_name='commented_project', on_delete=models.CASCADE)
+    comment = models.TextField(null=False, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return 'Comment by \'{}\' on \'{}\' at \'{}\''.format(self.user, self.project, self.created)
