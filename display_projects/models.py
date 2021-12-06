cd25885091e2d345db988d1e4e4a4672e026c44e
Shale Arkane <shalearkane@gmail.com>
2021-12-06 11:42:31 +0530

    added Project Rating (like/dislike)

--- display_projects/models.py
+++ display_projects/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 # Create your models here.
 
 
@@ -10,8 +11,24 @@ class ProjectModel(models.Model):
     screenshot = models.ImageField(upload_to="screenshots")
     description = models.TextField(default="Enter the description here")
     link = models.URLField(blank=True, null=True)
-    likes = models.PositiveSmallIntegerField(default=0)
-    dislikes = models.PositiveSmallIntegerField(default=0)
+    # likes = models.PositiveSmallIntegerField(default=0)
+    # dislikes = models.PositiveSmallIntegerField(default=0)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.creator.username + " - " + self.title
+
+
+class ProjectRating(models.Model):
+    user = models.ForeignKey(
+        User, related_name="user_rating", on_delete=CASCADE)
+    project = models.ForeignKey(
+        ProjectModel, related_name="rated_project", on_delete=CASCADE)
+
+    # like -> true, dislike -> false, neither -> null/delete row.
+    rating = models.BooleanField(null=True)
+
+    class Meta:
+        unique_together = ("user", "project")
+
+    def __str__(self) -> str:
+        return self.user.username + " - " + self.project.title + str(self.rating)
