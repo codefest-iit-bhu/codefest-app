45fd4ea66b47e6474c606178bfdfe36aeaa28715
AdiAkhileshSingh15 <adiakhilesh.singh.cse21@itbhu.ac.in>
2023-09-27 16:26:02 +0530

    Removes haxplore from navbar
    Signed-off-by: AdiAkhileshSingh15 <adiakhilesh.singh.cse21@itbhu.ac.in>

--- src/components/Menu/AppBar.vue
+++ src/components/Menu/AppBar.vue
@@ -7,15 +7,15 @@
           <!-- <span class="fa fa-circle fa-xs" :class="$style.awesome" aria-hidden="true"></span> -->
         </router-link>
       </li>
-      <li :class="$style.link" slot="left" v-if="['md', 'lg', 'xl', 'xxl'].includes(this.$mq)">
+      <!-- <li :class="$style.link" slot="left" v-if="['md', 'lg', 'xl', 'xxl'].includes(this.$mq)">
         <router-link to="/haxplore" v-if="currentPage != 'haxplore'">
           HaXplore
-          <!-- <span class="fa fa-circle fa-xs" :class="$style.awesome" aria-hidden="true"></span> -->
+          <span class="fa fa-circle fa-xs" :class="$style.awesome" aria-hidden="true"></span>
         </router-link>
-      </li>
+      </li> -->
       <li :class="$style.link" slot="left" v-if="['md', 'lg', 'xl', 'xxl'].includes(this.$mq)">
         <router-link to="/codestart" v-if="currentPage != 'codestart'">
-          Codestart
+          CodeStart
           <!-- <span class="fa fa-circle fa-xs" :class="$style.awesome" aria-hidden="true"></span> -->
         </router-link>
       </li>
@@ -108,15 +108,15 @@
                 <!-- <span class="fa fa-circle fa-xs" :class="$style.awesome" aria-hidden="true"></span> -->
               </router-link>
             </li>
-            <li :class="$style.link">
+            <!-- <li :class="$style.link">
               <router-link to="/haxplore" v-if="currentPage != 'haxplore'">
                 HaXplore
-                <!-- <span class="fa fa-circle fa-xs" :class="$style.awesome" aria-hidden="true"></span> -->
+                <span class="fa fa-circle fa-xs" :class="$style.awesome" aria-hidden="true"></span>
               </router-link>
-            </li>
+            </li> -->
             <li :class="$style.link">
               <router-link to="/codestart" v-if="currentPage != 'codestart'">
-                Codestart
+                CodeStart
                 <!-- <span class="fa fa-circle fa-xs" :class="$style.awesome" aria-hidden="true"></span> -->
               </router-link>
             </li>
